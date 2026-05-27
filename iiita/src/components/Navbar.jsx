3c045d75146ef9d87ef795a821d1376efd7839f2
KARTIKEYA SINGH <kartikeyasingh225@gmail.com>
2026-05-27 12:30:12 +0000

    navbar fix

--- iiita/src/components/Navbar.jsx
+++ iiita/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar() {
           
           <div className="hidden md:flex items-center gap-8 text-gray-700 font-medium">
             <a
-              href="#"
+              href="/#"
               className="hover:text-blue-900 transition"
             >
               Home
