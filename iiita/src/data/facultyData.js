d08db5b571eba8e178eeed47e58b508c759f3c65
KARTIKEYA SINGH <kartikeyasingh225@gmail.com>
2026-05-29 05:23:09 +0000

    Few Faculty added

--- iiita/src/data/facultyData.js
+++ iiita/src/data/facultyData.js
@@ -5,16 +5,55 @@ const facultyData = [
     name: "Prof. Sonali Agarwal",
 
     researchInterests: [
-      "Data Mining",
-      "Machine Learning",
-      "Big Data Analytics",
+      "Stream Analytics",
+      "Big Data",
+      "Artificial Intelligence",
       "Healthcare Analytics",
     ],
 
     email: "sonali@iiita.ac.in",
 
-    homepage: "profile.iiita.ac.in/sonali",
-  },
+    homepage: "https://profile.iiita.ac.in/sonali/",
+  },{
+    id:2,
+    image: "/faculty/anupam.jpg",
+    name: "Prof. Anupam Agrawal",
+
+    researchInterests: [
+      "Image & Vision Processing",
+      "Computer Graphics and Virtual Reality",
+      "Human-Computer Interaction",
+      "Multimedia Information Processing"
+    ],
+
+    email: "anupam@iiita.ac.in",
+
+    homepage: "https://profile.iiita.ac.in/anupam/",
+  },{
+    id:3,
+    image: "/faculty/pavan.jpg",
+    name: "Prof. Pavan Chakraborty",
+    researchInterests: [
+      "Human Gait Analysis",
+      "Human Prosthetics",
+      "Graphics and Visual Computing",
+      "Robotics & Instrumentation"
+    ],
+    email: "pavan@iiita.ac.in",
+    homepage: "https://it.iiita.ac.in/?pg=facultypage&uid=pavan",
+  },{
+    id:4,
+    image: "/faculty/anshu.jpg",
+    name: "Dr. Anshu S. Anand ,",
+    researchInterests: [
+      "Parallel Computing",
+      "Distributed Computing",
+      "High Performance Computing (HPC)",
+      "Computational Science"
+    ],
+    email: "anshu@iiita.ac.in",
+    homepage: "https://sites.google.com/view/anshusanand/",
+  }
 ]
 
 export default facultyData
